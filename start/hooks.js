f0abe7d569929f1a013f65f703509598d8836822
Leonardo Avelino <leonardo.avelino16@hotmail.com>
2021-03-02 19:58:06 -0300

    extends view method

--- start/hooks.js
+++ start/hooks.js
@@ -2,11 +2,16 @@ const { hooks } = require('@adonisjs/ignitor')
 
 hooks.after.providersBooted(() => {
   const Response = use('Adonis/Src/Response')
+  const View = use('View')
 
   Response.macro('sendStatusAndJson', function (status, json) {
     this.status(status).send({
       status,
       ...json
     })
+  })
+
+  View.global('currentTime', function () {
+    return new Date().getTime()
   })
 })
