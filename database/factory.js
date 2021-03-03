34ac44b96412d329de17911a21e4895c3900a090
Leonardo Avelino <leonardo.avelino16@hotmail.com>
2021-03-03 14:28:06 -0300

    Seeds & Factories

--- database/factory.js
+++ database/factory.js
@@ -12,10 +12,12 @@
 */
 
 /** @type {import('@adonisjs/lucid/src/Factory')} */
-// const Factory = use('Factory')
+const Factory = use('Factory')
 
-// Factory.blueprint('App/Models/User', (faker) => {
-//   return {
-//     username: faker.username()
-//   }
-// })
+Factory.blueprint('users', (faker, i, data) => {
+  return {
+    username: faker.username(),
+    password: data.prefix + faker.password(),
+    email: faker.email()
+  }
+})
